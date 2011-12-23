ab9aa88d45a42002f10c833fe85f0447f4259d29
Yi Liu <yinmotion.nyc@gmail.com>
2011-12-23 17:14:55 -0500

    updates

--- javascripts/main.js
+++ javascripts/main.js
@@ -14,24 +14,27 @@ $(document).ready(function(){
 	$log = $("#log");
 	$content = $(".content");
 	$vid = $("#vid");
+	$startArrow = $("#start_arrow")
 		
 	$content_top = $(".content_top");
 	
 	$log.click(function(){monitor=!monitor; if(!monitor){this.html("")}});
+	
+	$startArrow.click(init);
 });
 
-function transEnd(){
-	console.log("transEnd ")
-	if($content.css("left")>=-1 ){
-		$content.css("left", -content_w+2);
-	}
-	if($content.css("left")<=-content_w+1){
-		$content.css("left", -2);
-	}
-	$content.removeClass("content").addClass("content_no_easing");
-	setTimeout("resetTrans()", 1500);
-	
-};
+// function transEnd(){
+	// console.log("transEnd ")
+	// if($content.css("left")>=-1 ){
+		// $content.css("left", -content_w+2);
+	// }
+	// if($content.css("left")<=-content_w+1){
+		// $content.css("left", -2);
+	// }
+	// $content.removeClass("content").addClass("content_no_easing");
+	// setTimeout("resetTrans()", 1500);
+// 	
+// };
 
 function onVideoClick(){
 	if($(this).css("opacity")==0){
@@ -40,12 +43,29 @@ function onVideoClick(){
 	
 	if(this.paused){
 		this.play();
-	}else{
+	}else{ 
 		this.pause();
 	}
 }
 
+function resetTrans(){
+	//$log.html("reset trans");
+	$content.removeClass("content_no_easing").addClass("content");
+	$content_top.removeClass("content_no_easing").addClass("content_top");
+}
+
+function pauseTrans(){
+	paused = true;
+}
+
+function resumeTrans(){
+	paused = false;
+}
+
 function init(){
+	//console.log("init");
+	
+	$startArrow.remove();
 	
 	if (window.DeviceOrientationEvent) {
 	    window.addEventListener("deviceorientation", function () {
@@ -62,10 +82,14 @@ function init(){
 	    }, true);
 	}
 	
+	
+}
+
+window.onload = function(){
 	for(var i=0; i<6; i++){
 		var div = "<div class='top_elem' id='elem_"+i+"'></div>"
 		$content_top.append(div);
-		var elem = $("#elem_"+i).css({"top" : 150+Math.random()*300, "left": 150+Math.random()*7000, "width" : 60+Math.random()*600, "height" : 25+Math.random()*100, "background-color" : "rgba("+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+", .8)"});
+		var elem = $("#elem_"+i).css({"top" : 150+Math.random()*300, "left": 1000+Math.random()*7000, "width" : 60+Math.random()*600, "height" : 25+Math.random()*100, "background-color" : "rgba("+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+", .8)"});
 		
 		elem.click(function(){
 			$(this).css({"width" : 60+Math.random()*600, "height" : 25+Math.random()*100, "background-color" : "rgba("+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+", .8)"});
@@ -77,72 +101,69 @@ function init(){
 		})
 	}
 	
-	$("body").css("opacity", "1");	
 	
 	$vid.bind("touchend", onVideoClick);
 	$vid.bind("click", onVideoClick);
 	
-}
+	$("body").css("opacity", "1");	
+};
 
-window.onload = init;
-
+var curr_left;
 
 function tilt(alpha, beta, gamma){
-	if(!paused && alpha!=null){
-		if(loop){
-			var left =Math.round((360-alpha)/360*-content_w);
-			
-			//$log.html("log "+(alpha>=359));
-			if(alpha>=359.9 || alpha<=0.9){
-				$content.removeClass("content").addClass("content_no_easing");
-				$content_top.removeClass("content_top").addClass("content_no_easing");
-				
-				$content.css({"left": $content.css("left"), "top": $content.css("top")});
-				$content_top.css({"left": $content_top.css("left"), "top": $content_top.css("top")});
-				
-				if(alpha>=359.9){
-					$log.html("left end !!! "+left);
-					left = -content_w;
-				}else{
-					$log.html("right end!!! "+left);
-					left = 0;
-				}
-				
-				$content.css({"left": left, "top": top});
-				$content_top.css({"left": left, "top": top});
-				
-				//resetTrans();
-				setTimeout("resetTrans()", 500);
-			}
-		}else if(alpha<=358 && alpha>=2){
-			left=Math.round((360-alpha)/360*-content_w);
-		}
 
+	
+	$("#a_x").css("-webkit-transform", "rotateY("+alpha+"deg)");
+	$("#a_y").css("-webkit-transform", "rotateX("+beta+"deg)");
+	$("#a_z").css("-webkit-transform", "rotateZ("+gamma+"deg)");
+	
+	if(!paused && alpha!=null){
+		
 		if(gamma<0){
 			top =  Math.round((gamma + 90)/90*-offset_v);
 		}else{
 			top =  Math.round((90 - gamma)/90*-offset_v);
 		}
-
-		$content.css({"left": left, "top": top});
-		$content_top.css({"left": left, "top": top});
 		
+		if(loop){
+			//var left =(360-alpha)/360*-content_w;
+			
+			if(alpha>=359.5 || alpha<=0.5){
+				$content.removeClass("content").addClass("content_no_easing");
+				$content_top.removeClass("content_top").addClass("content_no_easing");
+// 				
+				// $content.css("-webkit-transform", "translate3d("+left+"px,"+top+"px,0)");
+				// $content_top.css("-webkit-transform", "translate3d("+left+"px,"+top+"px,0)");
+				
+				if(alpha<=0.001){
+					var left = -content_w;
+					//$log.html("left end !!! "+left);
+				}else if(alpha>=395.999) {
+					left = 0;
+					//$log.html("right end!!! "+left);
+				}
+				
+				$content.css("-webkit-transform", "translate3d("+left+"px,"+top+"px,0)");
+				$content_top.css("-webkit-transform", "translate3d("+left+"px,"+top+"px,0)");
+				setTimeout("resetTrans()", 2000);
+			}else{
+				//left =Math.round((360-alpha)/360*-content_w);
+			}
+			left =(360-alpha)/360*-content_w;
+		}else if(alpha<=360 && alpha>=0){
+			//TODO
+		}
+		
+		$content.css("-webkit-transform", "translate3d("+left+"px,"+top+"px,0)");
+		$content_top.css("-webkit-transform", "translate3d("+left+"px,"+top+"px,0)");
+		
+		var matrix = $content_top.css("-webkit-transform").substr(19);
+		matrix = matrix.substr(0, matrix.length-1);
+		var a = matrix.split(",")
+		$log.html(a[0]);
 		if(monitor){
 			$log.html("alpha : "+alpha + "<br> beta : "+beta + "<br> gamma : "+gamma + "<p>left  : "+left +"<br>top : "+top);
 		}
 	}
 }
 
-function resetTrans(){
-	$log.html("reset trans");
-	$content.removeClass("content_no_easing").addClass("content");
-	$content_top.removeClass("content_no_easing").addClass("content_top");
-}
-
-function pauseTrans(){
-	paused = true;
-}
-
-function resumeTrans(){
-	paused = false;
-}
